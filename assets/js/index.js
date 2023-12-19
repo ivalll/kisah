24a7d3773ada656be8a55f4e7c271b08d77e6dd4
ivalll <127493020+ivalll@users.noreply.github.com>
2023-12-19 21:10:19 +0700

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $(".custom-carousel .item").click(function () {
-    $(".custom-carousel .item").not($(this)).removeClass("active");
-    $(this).toggleClass("active");
-  });
-});
